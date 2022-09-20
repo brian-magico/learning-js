c5e4518c26d11b037938def8ab5f6047d78ef7a5
Brian Montoya <brianmontoya19@gmail.com>
2022-09-20 11:21:07 -0300

    ahora jcon java

---
+++ script1.js
@@ -0,0 +1,34 @@
+let numero1 = 8 
+
+
+let numero2 = 26
+
+let suma = numero1 + numero2
+
+// console.log("La suma es ", suma)
+// console.log(typeof(nombre))
+//tipos de datos logicos: true o false
+
+let valido = false
+let numero3 = "45"
+
+console.log(numero2 + parseInt(numero3))
+
+let nombre = "Pedro"
+let apellido ="Perez"
+
+let nombrecompleto = "El nombre completo es " + nombre + "" + apellido
+
+let fullname = `El nombre completo es ${nombre} ${apellido}`
+let resta = numero1 - numero2
+let multi = numero1 * numero2
+let dividido = numero1 / numero2
+let resto = 5 % 2; // el modulo es el resto de dividri u numero por otro
+
+let sum = `La suma de numero1 y numero2 es igual a ${numero1 + numero2}`
+
+// expresiones de comapracion (==, <, <=, =>, >, !=), siempre da como resultado un valor logico o booleano )o verdadero o falso)
+
+//Expresion logica
+// Y (and), O (or), NOT (!)
+const iva = 21;
