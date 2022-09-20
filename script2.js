67745738da4e379b2079b986056c9bcd14ec9886
Brian Montoya <brianmontoya19@gmail.com>
2022-09-20 11:28:57 -0300

    avers

--- script2.js
+++ script2.js
@@ -1,9 +1,13 @@
 // condicionales
-let edad = 18;
+let edad = 15;
 
-if (edad >= 18) { 
-    console.log("Es mayor de edad")
-} else {
-    console.log("Es menor de edad")
+if (edad >= 1 && edad <= 12) {
+    console.log("Es un ninio")
+} else if (edad >= 13 && edad <= 30) {
+    console.log("Es joven")
+} else if (edad >= 31 && edad <= 45) {
+    console.log("Es Adulto")
+} else { 
+    console.log("Es un viejo")
 }
 
